7a1e38658360dc561f501d12a1ec737d6b397f50
susmitha2981 <Susmithapanguluri2502@gmail.com>
2024-03-22 20:56:14 +0530

    feat - blogs added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
     <main>
       <ScrollUp />
       <Hero />
-      {/* <HomeBlogSection posts={posts} /> */}
+      <HomeBlogSection posts={posts} />
       {/* <Features /> */}
       {/* <About /> */}
       {/* <CallToAction /> */}
